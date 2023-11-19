d60bdcfc43de0950c7f06b2d0c44fe5a9956c394
Luca <lucagcuello@gmail.com>
2023-11-19 14:17:11 -0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import { NavBar } from "./components/Navbar";
+// TODO: Implementar fondo estrellado
 // import { StarryBackground } from "./components/StarryBackground";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import { AboutUs } from "./views/AboutUs";
 import { Hero } from "./views/Hero";
+import { Products } from "./views/Products";
 
 function App() {
   return (
     <>
-      <NavBar />
-      {/* <StarryBackground> */}
-      <Hero />
-      {/* </StarryBackground> */}
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/home" element={<Hero />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </Router>
     </>
   );
 }
