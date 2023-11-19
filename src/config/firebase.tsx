2b049a49de15fea16e11a6e27d0b12a38078ca9c
Luca <lucagcuello@gmail.com>
2023-11-19 20:55:21 -0300

    Add firebase dependency

---
+++ src/config/firebase.tsx
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_BnNLrIvZdQwDAxpfqS-A6B1Grrj19ok",
+  authDomain: "galaxystore-36f85.firebaseapp.com",
+  projectId: "galaxystore-36f85",
+  storageBucket: "galaxystore-36f85.appspot.com",
+  messagingSenderId: "889476586606",
+  appId: "1:889476586606:web:da401199fc6ce839fc9a7f",
+  measurementId: "G-QVKFJBX7ZX",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
